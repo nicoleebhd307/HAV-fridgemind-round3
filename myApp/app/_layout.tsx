2338645d106f7385dbec9098e9f0a2d56bbdfce4
Hoang Dieu <dieubh234112e@st.uel.edu.vn>
2026-01-13 00:16:57 +0700

    update: delete default files of react-native

--- myApp/app/_layout.tsx
+++ myApp/app/_layout.tsx
@@ -17,7 +17,6 @@ export default function RootLayout() {
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="scan" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
